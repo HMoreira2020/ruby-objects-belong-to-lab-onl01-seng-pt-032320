814af6c4c4694c973c2f5e0714285e737d84f71e
Heather Moreira <moreiraheather@gmail.com>
2020-04-08 00:25:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,8 @@
 class Song 
   attr_accessor :title, :artist 
 
-  def initialize(title)
-    @title = title 
-  end 
-  
 
 end 
 
-beyonce = Artist.new("Beyonce")
-song = Song.new("7/11")
-song.artist = beyonce  
+song = Song.new
+
